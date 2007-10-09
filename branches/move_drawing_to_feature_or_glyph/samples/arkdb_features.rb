b20a1a1a7a47d9672aac461a8491a8beddd522c7
jandot <jandot>
2007-10-09 19:52:55 +0000

    Creating branch to move drawing code to feature or glyph
    git-svn-id: svn+ssh://rubyforge.org/var/svn/bio-graphics@12 a2f46d20-7dc0-45cf-9d05-bfa4e4ff58eb

---
+++ branches/move_drawing_to_feature_or_glyph/samples/arkdb_features.rb
@@ -0,0 +1,37 @@
+require File.dirname(__FILE__) + '/../lib/bio-graphics'
+
+#Initialize graphic for a nucleotide sequence of 4173015 bp, zooming in on the
+#region 11111..3333333
+my_panel = Bio::Graphics::Panel.new(4173015, 800, false, 11111, 3333333)
+#my_panel = Bio::Graphics::Panel.new(4173015, 800, false, 1, 4173015)
+
+#Create and configure tracks
+scaffold_track = my_panel.add_track('scaffold')
+marker_track = my_panel.add_track('marker')
+clone_track = my_panel.add_track('clone')
+
+scaffold_track.feature_colour = [1,0,0]
+marker_track.feature_colour = [0,1,0]
+marker_track.feature_glyph = 'triangle'
+clone_track.feature_colour = [0,0,1]
+
+# Add data to tracks
+File.open('data.txt').each do |line|
+  line.chomp!
+  accession, type, start, stop = line.split(/\t/)
+  if type == 'scaffold'
+    if start.nil?
+      scaffold_track.add_feature(accession)
+    else
+      scaffold_track.add_feature(accession, start + '..' + stop)
+    end
+    
+  elsif type == 'marker'
+    marker_track.add_feature(accession, ((start.to_i + stop.to_i)/2).to_s, 'http://www.thearkdb.org/arkdb/do/getMarkerDetail?accession=' + accession)
+  elsif type == 'clone'
+    clone_track.add_feature(accession, start + '..' + stop)
+  end
+end
+
+# And draw
+my_panel.draw('my_panel.png')
